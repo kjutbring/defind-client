0a3cd518194d104dfd71eefcf80c04935bf5af32
kitty jutbring <kittyj@cmail.nu>
2016-01-11 13:31:42 +0100

    add Location,java

---
+++ app/src/main/java/com/defind/kittyj/defind_client/Location.java
@@ -0,0 +1,47 @@
+package com.defind.kittyj.defind_client;
+
+/**
+ * Created by kittyj on 1/11/16.
+ */
+public class Location {
+
+    private String device;
+    private String lat;
+    private String lng;
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDevice() {
+        return device;
+    }
+
+    public void setDevice(String device) {
+        this.device = device;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+
+    private String time;
+
+
+}
